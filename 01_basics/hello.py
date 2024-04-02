80895ddf1255aa74c2231c11637b7599786124a9
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-04-03 00:16:37 +0530

    "baisc_of_python"

---
+++ 01_basics/hello.py
@@ -0,0 +1,7 @@
+print("Hello, cadt")
+
+def chai(n):
+    print(n)
+
+chai(4)
+chai("lemon Tea")
---
+++ 01_basics/working.py
@@ -0,0 +1,15 @@
+# code - byte code - python VM (virtual machine)
+
+# Compile to byte code (low level and platform independent)
+# Byte code runs faster
+# .pyc -> compiled python (frozen binaries)
+# __pycache__
+
+# PVM
+# code loop to iterate byte code
+# run time engine
+# also known as python interpreter
+
+# Byte code is not machine code
+# python specific interpretation
+# cpython, jython, Iron python, stackless, pypy 
