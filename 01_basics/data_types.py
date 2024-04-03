f3d85186e41e75d94ba110fd291969fa49e0062c
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-04-03 18:05:28 +0530

    "dataTypes, mutable and numbers in depth"

---
+++ 01_basics/data_types.py
@@ -0,0 +1,24 @@
+# data types / object types
+
+# - Number
+# - String
+# - List
+# - Tuple
+# - Dictionary
+# - Set
+
+
+# - Boolean
+# - None
+# - Function
+# - Modules
+# - Classes
+
+# - Advanced - Decorators, Generators, Iterators, MetaProgramming
+
+
+
+
+
+# - File
+
---
+++ 01_basics/mutable_and_unmutable.py
@@ -0,0 +1,2 @@
+# Mutable -> list, set, dictionary, bytearray, array
+# Immutable -> integers, floting-point numbers, boolean, strings, tuples, frozen set, bytes
